5ec71dd40c6f9fe7e11956f062f60d681d84bb72
Edgar Gomes <edgargomes.iff@gmail.com>
2022-02-11 17:39:54 -0300

    Validação de campos do formulário adicionadas.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,27 @@ function mask(e){
 		e.preventDefault()
 	}
 }
+function testInputs(e){
+	e.preventDefault()
+	var tt = document.getElementById('transaction-type').value
+	var nm = document.getElementById('merch-name').value
+	var vl = document.getElementById('value').value.replace('.','').replace(',','.')
+	console.log(tt,nm,vl)
+	if(nm == ''){
+		alert('Por favor, insira um nome para a mercadoria')
+		document.getElementById('merch-name').focus()
+		return false
+	}
+	
+	if(vl == ''){
+		alert('Por favor, insira um valor para a transação!')
+		document.getElementById('value').focus()
+		return false
+	} else {
+			if(vl.indexOf('.') == -1){
+				alert('Favor, adicionar a quantia em centavos!')
+				document.getElementById('value').focus()
+				return false
+			}
+		}
+}
